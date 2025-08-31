6074e574f64ed20fd765830a0bcc4bc5d054c1da
dinesh45obito <dineshrsdk45@gmail.com>
2025-08-31 07:48:21 +0530

    Add files via upload

---
+++ pattern7.java
@@ -0,0 +1,9 @@
+    class pattern7{
+     public static void main(String [] args){
+        int n=4;
+        for (int i=1;i<=n;i++){
+            for (int j=n-i+1;j>=1;j--){
+                System.out.print(+j);
+            }System.out.println(" ");
+        }
+     }    }
